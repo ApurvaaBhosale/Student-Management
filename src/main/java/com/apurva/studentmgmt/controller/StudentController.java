4b1f8f6ad729d28847138528a7891f31fd0e47db
ApurvaaBhosal <apurvabhosale15504@gmail.com>
2025-05-27 16:07:32 +0530

    Initial Commit

---
+++ src/main/java/com/apurva/studentmgmt/controller/StudentController.java
@@ -0,0 +1,11 @@
+package com.apurva.studentmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentController {
+    public static void main(String[] args){
+        SpringApplication.run(StudentController.class, args);
+    }
+}
